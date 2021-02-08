0c7529563a2bfe0fcecaf506c11908fb27482f8d
Hardik KAlasua <hardik.kalasua@gmail.com>
2021-02-09 01:38:49 +0530

    Initial commit

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo/helpers/database_helper.dart';
+import 'package:flutter_todo/models/task_model.dart';
+import 'package:intl/intl.dart';
+
+class AddTaskScreen extends StatefulWidget {
+  final Task task;
+
+  const AddTaskScreen({this.task});
+
+  @override
+  _AddTaskScreenState createState() => _AddTaskScreenState();
+}
+
+class _AddTaskScreenState extends State<AddTaskScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _title = '';
+  String _priority;
+  DateTime _date = DateTime.now();
+  TextEditingController _dateController = TextEditingController();
+
+  final DateFormat _dateFormat = DateFormat('MMM dd, yyyy');
+  final List<String> _priorities = ['Low', 'Medium', 'High'];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    if (widget.task != null) {
+      _title = widget.task.title;
+      _date = widget.task.date;
+      _priority = widget.task.priority;
+    }
+  }
+
+  @override
+  void dispose() {
+    _dateController.dispose();
+    super.dispose();
+  }
+
+  _handleDatePicker() async {
+    final DateTime date = await showDatePicker(
+        context: context,
+        initialDate: _date,
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2100));
+    if (date != null && date != _date) {
+      setState(() {
+        _date = date;
+      });
+      _dateController.text = _dateFormat.format(date);
+    }
+  }
+
+  _submit() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      print('$_title, $_date, $_priority');
+
+      Task task = Task(title: _title, date: _date, priority: _priority);
+      if (widget.task == null) {
+        task.status = 0;
+        DatabaseHelper.instance.updateTask(task);
+      } else {
+        task.status = widget.task.status;
+        DatabaseHelper.instance.updateTask(task);
+      }
+
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 80.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                GestureDetector(
+                  onTap: () => Navigator.pop(context),
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    size: 30.0,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  'Add Task',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 20.0),
+                          child: TextFormField(
+                            style: TextStyle(fontSize: 18.0),
+                            decoration: InputDecoration(
+                                labelText: 'Title',
+                                labelStyle: TextStyle(fontSize: 18.0),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                )),
+                            validator: (input) => input.trim().isEmpty
+                                ? 'Please enter a task title'
+                                : null,
+                            onSaved: (input) => _title = input,
+                            initialValue: _title,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 20.0),
+                          child: TextFormField(
+                            readOnly: true,
+                            controller: _dateController,
+                            style: TextStyle(fontSize: 18.0),
+                            onTap: _handleDatePicker,
+                            decoration: InputDecoration(
+                                labelText: 'Date',
+                                labelStyle: TextStyle(fontSize: 18.0),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                )),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 20.0),
+                          child: DropdownButtonFormField(
+                            isDense: true,
+                            icon: Icon(Icons.arrow_drop_down_circle),
+                            iconSize: 22.0,
+                            iconEnabledColor: Theme.of(context).primaryColor,
+                            style: TextStyle(fontSize: 18.0),
+                            items: _priorities.map((String priority) {
+                              return DropdownMenuItem(
+                                value: priority,
+                                child: Text(
+                                  priority,
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 18.0),
+                                ),
+                              );
+                            }).toList(),
+                            decoration: InputDecoration(
+                                labelText: 'Priority',
+                                labelStyle: TextStyle(fontSize: 18.0),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                )),
+                            validator: (input) => _priority == null
+                                ? 'Please select a priority level.'
+                                : null,
+                            onChanged: (value) {
+                              setState(() {
+                                _priority = value;
+                              });
+                            },
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.symmetric(vertical: 20.0),
+                          height: 60.0,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          child: FlatButton(
+                            child: Text(
+                              'Add',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 20.0),
+                            ),
+                            onPressed: _submit,
+                          ),
+                        )
+                      ],
+                    ))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
